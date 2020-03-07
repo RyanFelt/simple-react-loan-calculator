c2e41607435219b4e80a9e27ca8c6e855b51e316
RyanFelt <ryanwfeltkamp@gmail.com>
2020-03-06 20:18:41 -0500

    clean up

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Simple React Loan Calculator
 
 Goal was to build out a simple working loan calculator as fast and possible
+<<<<<<< Updated upstream
 
 Demo: http://simple-react-loan-calculator.s3-website-us-east-1.amazonaws.com/
+=======
+>>>>>>> Stashed changes
--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [amount, setAmount] = useState(100000);
+  const [rate, setRate] = useState(8);
+  const [period, setPeriod] = useState(10);
+
+  useEffect(() => {
+    math();
+  }, [amount, rate, period]);
+
+  const [monthlyPayment, setMonthlyPayment] = useState(466);
+  const [totalCost, setTotalCost] = useState(151777);
+
+  const handleAmountChange = event => {
+    const cleanAmount = removeCommas(event.target.value);
+    setAmount(cleanAmount);
+  };
+
+  const handleRateChange = event => {
+    setRate(event.target.value);
+  };
+
+  const handlePeriodChange = event => {
+    setPeriod(event.target.value);
+  };
+
+  const math = () => {
+    if (!period) return setMonthlyPayment(0);
+
+    const months = period * 12;
+
+    setMonthlyPayment(Math.round(amount / months));
+
+    if (!rate || parseInt(rate) === 0) {
+      setTotalCost(amount);
+    } else {
+      const rateDecimal = rate / 1200;
+
+      const top = Math.round(rateDecimal * amount * months);
+      const bottom = 1 - Math.pow(1 + rateDecimal, -months);
+      setTotalCost(Math.round(top / bottom));
+    }
+  };
+
+  const addCommas = x => {
+    if (!x) return '';
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  const removeCommas = x => {
+    return parseInt(x.replace(/,/g, ''));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-item">
+        <h3>Loan Calcuator</h3>
+        <div className="flex-container">
+          <div className="flex-item">
+            <sup>Amount</sup>
+            <input
+              className="input"
+              value={addCommas(amount)}
+              onChange={handleAmountChange}
+            ></input>
+          </div>
+
+          <div className="flex-item">
+            <sup>Interest Rate (%)</sup>
+            <div>
+              <input
+                className="input"
+                value={rate}
+                onChange={handleRateChange}
+              ></input>
+            </div>
+          </div>
+
+          <div className="flex-item">
+            <sup>Period (years)</sup>
+            <div>
+              <input
+                className="input"
+                value={period}
+                onChange={handlePeriodChange}
+              ></input>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-container">
+          <div>
+            <sup>Total Loan Cost &nbsp; ${addCommas(totalCost)}</sup>
+          </div>
+        </div>
+
+        <div className="flex-container">
+          <div>Monthly Payment &nbsp; ${addCommas(monthlyPayment)}</div>
+        </div>
+      </div>
     </div>
   );
 }
